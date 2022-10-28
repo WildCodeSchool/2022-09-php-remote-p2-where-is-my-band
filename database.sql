57971500183ffe8f444d9e132a616fc03617be27
Hugo David <hugodvd@outlook.com>
2022-10-28 11:02:23 +0200

    form + ajout database

--- database.sql
+++ database.sql
@@ -17,27 +17,101 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `simple-mvc`
+--
 --
 
+CREATE DATABASE whereismyband;
+USE whereismyband;
 -- --------------------------------------------------------
 
 --
--- Structure de la table `item`
+-- Structure de la table `user`
 --
 
-CREATE TABLE `item` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `user` (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+`password` varchar(20),
+email VARCHAR(255),
+nickname VARCHAR(80)
+);
 
+-- Structure de la table `localisation`
 --
+
+CREATE TABLE localisation (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+region varchar(100),
+department VARCHAR(100),
+);
+
+-- Structure de la table `band`
+--
+
+CREATE TABLE band (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+name varchar(80) NOT NULL,
+`description` TEXT NOT NULL,
+picture VARCHAR(255);
+CONSTRAINT fk_band_localisation
+FOREIGN KEY (localisation_id)
+REFERENCES localisation(id)
+);
+
+-- SELECT region, department name
+-- FROM localisation
+-- INNER JOIN band ON band.id=localisation.band_id;
+-- -- Structure de la table `search_instrument`
+-- --
+
+CREATE TABLE search_instrument (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+created_at DATETIME,
+`description` TEXT,
+CONSTRAINT fk_instrument_searchinstrument
+FOREIGN KEY (instrument_id)
+REFERENCES instrument(id)
+);
+
+-- Structure de la table `instrument`
+--
+
+CREATE TABLE instrument (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+`name` VARCHAR(255),
+CONSTRAINT fk_instrument_category
+FOREIGN KEY (category_id)
+REFERENCES category(id)
+);
+
+-- Structure de la table `category`
+--
+
+CREATE TABLE category (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+`name` VARCHAR(100),
+);
+--
+
+-- Structure de la table `musicians`
+--
+
+CREATE TABLE musicians (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+firstname varchar(80),
+lastname varchar(80),
+adress VARCHAR(255);
+CONSTRAINT fk_musicians_instrument FOREIGN KEY (instrument_id) REFERENCES instrument(id),
+CONSTRAINT fk_musicians_localisation FOREIGN KEY (localisation_id) REFERENCES localisation(id),
+);
 -- Contenu de la table `item`
 --
 
-INSERT INTO `item` (`id`, `title`) VALUES
+INSERT INTO `user` (id, password, email, nickname) VALUES
 (1, 'Stuff'),
-(2, 'Doodads');
+(2, 'password'),
+(3, `test@gmail.com`),
+(4, 'Stuff');
+
 
 --
 -- Index pour les tables exportées
@@ -46,7 +120,7 @@ INSERT INTO `item` (`id`, `title`) VALUES
 --
 -- Index pour la table `item`
 --
-ALTER TABLE `item`
+ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
